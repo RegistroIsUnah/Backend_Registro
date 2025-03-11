cf0174df7792d1a84fd5b98301119dfd0d2a50a6
Jose F <jfvargascarrasco03@gmail.com>
2025-03-11 17:27:44 -0600

    Obtener lista de espera-comentario

---
+++ src/api/get/listas_de_espera.php
@@ -0,0 +1,26 @@
+<?php
+
+// Archivo: src/api/get/listas_de_espera.php
+
+/**
+ * API para obtener las listas de espera de cada sección.
+ * 
+ * @author Jose Vargas
+ * @version 1.0
+ *
+ * Se espera recibir el  () mediante el parámetro () en la query string.
+ * La respuesta incluye las listas de espera de las secciones gestionadas por el jefe de departamento.
+ *
+ * Ejemplo de URL:
+ * servidor:
+ *
+ * Respuestas HTTP:
+ * - 200 OK: Listas de espera encontradas y se retorna la información en formato csv.
+ * - 400 Bad Request: Falta el parámetro jefeDepartamentoId o es inválido.
+ * - 404 Not Found: No se encontraron listas de espera para el jefe de departamento.
+ * - 500 Internal Server Error: Error en la consulta.
+
+ */
+
+?>
+
