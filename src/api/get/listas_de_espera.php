d1e7236596a3a463a4edd3195d7edafb2b8cf025
Jose F <jfvargascarrasco03@gmail.com>
2025-03-16 00:25:45 -0600

    Refactorizar la funcion  ClaseDepartamento y Agregar generar CSV de Aspirantes

--- src/api/get/listas_de_espera.php
+++ src/api/get/listas_de_espera.php
@@ -46,7 +46,7 @@
 header('Content-Type: application/json');
 
 
-require_once __DIR__ . '/../../controllers/ListasEsperaController.php';
+require_once __DIR__ . '/../../controllers/MatriculaController.php';
 
 
 $controller = new MatriculaController();
