52e7176e5a9866cf54d19a95343710d8a74d4182
IIIIIIIIIIIII <jfvargascarrasco03@gmail.com>
2025-04-09 11:59:15 -0600

    Solicitudes por tipo(Nombre)

---
+++ src/api/get/obtener_solicitud_tipo.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * API para obtener solicitudes filtradas por tipo
+ * 
+ * Método: GET
+ * Ejemplo:
+ * http://localhost/Backend_Registro/src/get/obtener_solicitud_tipo.php?tipo_solicitud=CAMBIO_CENTRO
+ */
+
+ header("Access-Control-Allow-Origin: *");
+ header('Content-Type: application/json');
+ 
+ if (!isset($_GET['tipo_solicitud']) || empty(trim($_GET['tipo_solicitud']))) {
+     http_response_code(400);
+     echo json_encode(['error' => 'El parámetro tipo_solicitud es requerido']);
+     exit;
+ }
+ 
+ $tipoSolicitud = $_GET['tipo_solicitud'];
+ 
+ require_once __DIR__ . '/../../controllers/SolicitudController.php';
+ 
+ $controller = new SolicitudController($conn);
+ $controller->obtenerSolicitudesPorTipo($tipoSolicitud);
+?>
